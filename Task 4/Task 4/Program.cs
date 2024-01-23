fd4d974424fc1a407ba32f94ea8e0f8b7f5efd20
garbich <131876643+garbich@users.noreply.github.com>
2024-01-23 22:35:33 +0200

    Add files via upload

---
+++ Task 4/Task 4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MySpace
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Person person = new Person("Artyr", 17);
+           
+            Console.WriteLine(person.Name + " " + person.Age);
+
+            person.Age = 16;
+
+            Console.WriteLine(person.Age);
+        }
+    }
+}
