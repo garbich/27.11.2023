259ea09e3c149b4b11e4b633323a0247d3a0a2ff
garbich <131876643+garbich@users.noreply.github.com>
2024-01-23 22:40:40 +0200

    Add files via upload

---
+++ Task 5/Task 5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MySpace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            Console.WriteLine("input world with a space");
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            list.AddRange(words);
+
+
+            list.RemoveAll(list => list.Length > 3);
+            Console.WriteLine("Count of element: " + list.Count);
+            foreach (string word in list)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
